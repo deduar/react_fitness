eca5e3645a9697c800b178ba86044ac86c139c7d
deduar <deduar@110918>
2020-04-03 11:37:02 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+const element = document.createElement('h1')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+element.innerText = 'Hello React'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('root')
+
+container.appendChild(element)
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const nombre = 'Eduardo'
+const user = {
+  firstName: 'Eduardo',
+  lastName: 'Diez',
+  avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y'
+}
+function getName(user){
+  return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+  if(user){
+    return <h1>Hello World !!! {getName(user)} </h1>
+  } 
+  return <h1>Hello World !!!  </h1>
+}
+
+const element = (
+  <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
+)
+
+const container = document.getElementById('root')
+
+ReactDOM.render(element, container)
+
